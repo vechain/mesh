927d164bd180d209ba1bd5f1f3d7b9ca4a6d0165
freemanzMrojo <miguel.rojo@vechain.org>
2025-10-14 14:39:49 +0100

    first commit

--- tests/constants.go
+++ tests/constants.go
@@ -2,7 +2,9 @@ package tests
 
 const (
 	FirstSoloAddress = "0xf077b491b355e64048ce21e3a6fc4751eeea77fa"
-	TestAddress1     = "0x16277a1ff38678291c41d1820957c78bb5da59ce"
+
+	TestAddress1PrivateKey = "5959365666b6165dbbbe6c406d1f07f5f430bab3064c5ea89ff0a0edd8ff5e19"
+	TestAddress1           = "0x16277a1ff38678291c41d1820957c78bb5da59ce"
 
 	// This is the address used when simulating contract deployment in Thor
 	SimulatedContractAddress = "0x841a6556c524d47030762eb14dc4af897e605d9b"
